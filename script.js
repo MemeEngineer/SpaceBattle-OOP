72855c5d2cda5960795e138075082bcaba9b132d
JWu <johnnyw131@gmail.com>
2023-07-16 23:05:30 -0400

    Functional MVP

--- script.js
+++ script.js
@@ -20,19 +20,29 @@ class HumanShip extends Ship{
     attack(Alien){
         if(Math.random() < this.accuracy){
             console.log(`You have hit the alien`)
-            Alien[i].hull -= this.firepower
+            Alien.hull -= this.firepower
 
-            Alien[i].hull <= 0 ? console.log(`Alien ship has been destroyed`) : console.log(`The Alien has ${Alien[i].hull} hull points remaining`)
+            Alien.hull <= 0 ? console.log(`Alien ship has been destroyed there are still ${alienDock.length -1} aliens on the dock`) : console.log(`The Alien has ${Alien.hull} hull points remaining there are ${alienDock.length}  ships left`)
+
+            if(Alien.hull <= 0){
+                delAlien()
+                console.log(Alien)
+                console.log(alienDock)
+            }
         }else{
             console.log(`Noob, Your attack has missed!`)
         }
+    }
+
+    retreat(){
+        console.log(`You have retreated`)
     }
 }
 
 class AlienShip extends Ship{
     constructor(hull=1, firepower=2,accuracy=.3){
         super(hull, firepower,accuracy)
-        this.hull = Math.floor(Math.random() * 4) + 3;
+        this.hull = (Math.floor(Math.random() * 4) + 3)
         this.firepower= Math.floor(Math.random() * 3) + 2;
         this.accuracy=(Math.floor(Math.random() * 3) + 6) / 10;
     }
@@ -41,7 +51,7 @@ class AlienShip extends Ship{
             console.log(`AlienShip has hit your ship!`)
         ussAssembly.hull -= this.firepower
 
-        ussAssembly.hull <= 0 ? console.log(`Oh boy! Get Good because you have ${ussAssembly.hull}`) : console.log(`You have ${ussAssembly.hull} hull points remaining`)
+        ussAssembly.hull <= 0 ? console.log(`Oh boy! Get Good because you have ${ussAssembly.hull} hull points remaining`) : console.log(`You have ${ussAssembly.hull} hull points remaining`)
         
         }else{
             console.log(`Blast! I have missed, prepare for another! ARGH!`)
@@ -56,30 +66,67 @@ const gameObject= {
     checkWin(){
         if(ussAssembly.hull <= 0){
                 console.log("Game Over! Your ship has exploded.")
+                removeAlienSet()
+                setAlienDock()
+            const  ussAssembly = new HumanShip(20, 5, .7 )
         }
 
+        if(alienDock.length === 0){
+            console.log('Congradulations you have saved the world!')
+            setAlienDock()
+            const  ussAssembly = new HumanShip(20, 5, .7 )
+
+        }
 
        
     }
 }
-// console.log(gameObject.checkWin())
 
 
-const  ussAssembly = new HumanShip(20, 5, .7 )
+ const  ussAssembly = new HumanShip(20, 5, .7 )
 
-console.log(ussAssembly)
+// console.log(ussAssembly)
 
-const Alien1 = new AlienShip (1, 2, .3)
+// const Alien1 = new AlienShip (1, 2, .3)
 
-console.log(Alien1)
+// console.log(Alien1)
 
-const Alien =[]
+const atkbtn = document.querySelector('#atk')
+
+atkbtn.addEventListener('click', function(){
+    console.log(ussAssembly)
+   ussAssembly.attack(alienDock[0])
+   gameObject.checkWin()
+   alienDock[0].attack(ussAssembly)
+   
+})
+
+const retreat = document.querySelector('#retreat')
+// console.log(retreat)
+
+retreat.addEventListener('click', function (){
+    ussAssembly.retreat()
+})
+
+function delAlien (){
+    alienDock.splice(0,1)
+}
+const alienDock =[]
+function setAlienDock(){
+   
 for( let i = 0; i < 6; i++){
     const alienship = new AlienShip()
-    Alien.push(alienship)
-    console.log(Alien)
+    alienDock.push(alienship)
+    
+}
 }
 
+function removeSetAlien(){
+     alienDock= []
+}
 
+const start = document.querySelector('#start')
 
-
+start.addEventListener('click', function(){
+    setAlienDock()
+})
